939a1428c7abd22ee84fbf0f356d87af3c0205c1
JBar0 <baranowski.jakub97@gmail.com>
2021-03-21 20:25:59 +0100

    init

---
+++ src/test/java/com/example/unity/UnityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
