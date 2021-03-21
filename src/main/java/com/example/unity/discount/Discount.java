a9ea40973a9a6f2a36ab7a304efe504d449cfaee
JBar0 <baranowski.jakub97@gmail.com>
2021-03-21 20:35:52 +0100

    discount service created

---
+++ src/main/java/com/example/unity/discount/Discount.java
@@ -0,0 +1,9 @@
+package com.example.unity.discount;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Discount {
+
+
+}
---
+++ src/test/java/com/example/unity/discount/DiscountTest.java
@@ -0,0 +1,21 @@
+package com.example.unity.discount;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscountTest {
+
+    Discount discount;
+
+    @BeforeEach
+    public void setUp() {
+        discount = new Discount();
+    }
+
+    @Test
+    public void givenTwoProducts_thenReturnThemWithCalculatedDiscounts() {
+
+    }
+}
